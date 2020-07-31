25beb80a87180946270a688e4a5df79ff0ed93b1
JUSELERO <juansebastianleon5@gmail.com>
2020-07-31 08:50:05 -0500

    segunda opc

---
+++ operador.php
@@ -0,0 +1,18 @@
+<html>
+    <meta http-eqiv="content-type" content="text/html"; charset="utf-8">
+    <head>
+<title>operadores</title>     
+</head>
+<body>
+    <center>
+    echo "hello mad";
+    $m = echo rand(0,20). "\n";
+    $n = echo rand(0,20). "\n";
+    echo max($m, $n)
+    <?php
+
+    ?>
+    </center>
+</body>
+
+</html>
